6200da1cd86d0210bbfdf1c200cddaf1aa1a9808
stfDeveloper <sstefancosmin@icloud.com>
2022-01-30 18:48:16 +0100

    finito + bonus

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title/>
+    <Jumbo/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
+import Jumbo from '/components/Jumbo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Title,
+    Jumbo,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
